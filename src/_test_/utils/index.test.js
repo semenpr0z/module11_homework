f250da2909900ca715543069f3e3564a08b86593
Semen <justpr0z.com.ru@gmail.com>
2022-12-30 22:59:43 +0300

    added files

---
+++ src/_test_/utils/index.test.js
@@ -0,0 +1,17 @@
+import {repeatWord}  from "../../utils/index.js";
+
+test('Output a string with a given number for its repetition, which are passed in the arguments repeatWord(word, number)' () => {
+    expect(repeatWord('слово', 2)).toBe( 'слово1, слово2, ');
+});
+
+xtest('Output a string with a given number for its repetition, which are passed in the arguments repeatWord(word, number)', () => {
+	expect(repeatWord('слово', 2)).toBe( 'слово1, слово2,');
+});
+
+describe('Output a string with a given number for its repetition, which are passed in the arguments repeatWord(word, number)', () => {
+	it('The function should return the received word and the number of times to repeat it', () => {
+	  expect(repeatWord('слово', 1)).toBe('слово1, ');
+	  expect(repeatWord('слово', 2)).toBe('слово1, слово2, ');
+	  expect(repeatWord('слово', 3)).toBe('слово1, слово2, слово3, ');
+	});
+  });
